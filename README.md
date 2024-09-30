44080ff208cc4f97999489198423f5fa81cbd8bb
kirstinlyon <kirstin@mltwelve.com>
2024-09-30 05:25:48 +0200

    Initial set-up

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# budget_qfr
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of budget_qfr is to ...
+
+
+---
+
+*Disclaimer: The findings, interpretation, and conclusions expressed herein are those of the authors and do not necessarily reflect the views of United States Agency for International Development. All errors remain our own.*
---
+++ Scripts/setup_file.R
@@ -0,0 +1,22 @@
+required_packages <- c("dplyr","purrr", "tidyr","stringr", "janitor", "lubridate", "glamr",
+                       "readxl", "readr")
+
+missing_packages <- required_packages[!(required_packages %in% installed.packages()
+                                        [,"Package"])]
+
+if(length(missing_packages) >0){
+    install.packages(missing_packages, repos = c("https://usaid-oha-si.r-universe.dev",
+                                                 "https://cloud.r-project.org"))
+}
+
+
+library(glamr)
+
+# OTHER SETUP  - only run one-time --------------------------------------
+
+folder_setup() 
+folder_setup(folder_list = list("Data/subobligation_summary", 
+                                "Data/active_awards", 
+                                "Data/phoenix_transactions", 
+                                "Data/phoenix_pipeline",
+                                "Data/close_out_tracker"))
